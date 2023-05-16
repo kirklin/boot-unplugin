479e33eb93e71be2af8f7fc0d9b81c7b11104fb6
Kirk Lin <linkirk@163.com>
2023-05-16 19:13:06 +0800

    chore: update deps

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment */
 import type { Options } from "./types";
 import unplugin from ".";
+
 export default function (options: Options = {}, nuxt: any) {
   // install webpack plugin
   nuxt.hook("webpack:config", (config: any) => {
